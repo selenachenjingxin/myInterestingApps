30cd679b7a3e91fff94a86b4a779f61d6a25cd3e
陈景欣 <jingxinchen281@gmail.com>
2023-05-10 19:02:52 +0800

    Update countDown1.py

--- countDown1.py
+++ countDown1.py
@@ -4,9 +4,10 @@ from streamlit import session_state as SessionState
 
 # 定义一个计算倒计时的函数
 def calculate_countdown(event_date):
-    today = datetime.date.today()
-    days_remaining = (event_date - today).days
-    return days_remaining
+    today = datetime.datetime.now()
+    time_remaining = event_date - today
+    seconds_remaining = time_remaining.total_seconds()
+    return int(seconds_remaining)
 
 # 初始化SessionState
 if not SessionState.__contains__('events'):
@@ -16,7 +17,7 @@ if not SessionState.__contains__('events'):
 st.title("倒计时小程序")
 
 # 侧边栏
-st.sidebar.title("添加新倒计时")
+st.sidebar.header("添加新倒计时")
 event_name = st.sidebar.text_input("事件名称")
 min_date = datetime.date.today()
 event_date = st.sidebar.date_input("事件日期", min_date)
@@ -35,7 +36,6 @@ if st.sidebar.button("删除倒计时"):
         st.experimental_rerun()
 
 # 显示所有倒计时事件
-st.header("所有倒计时")
 
 for event_name, event_date in SessionState.events.items():
     countdown = calculate_countdown(event_date)
