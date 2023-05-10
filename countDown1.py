2cc4d687dd757c49b0f6973e4d4d1bb0499235ba
陈景欣 <jingxinchen281@gmail.com>
2023-05-10 19:06:08 +0800

    Update countDown1.py

--- countDown1.py
+++ countDown1.py
@@ -17,26 +17,29 @@ if not SessionState.__contains__('events'):
 st.title("倒计时小程序")
 
 # 侧边栏
-st.sidebar.header("添加新倒计时")
+st.sidebar.title("添加新倒计时")
 event_name = st.sidebar.text_input("事件名称")
 min_date = datetime.date.today()
 event_date = st.sidebar.date_input("事件日期", min_date)
+event_time = st.sidebar.time_input("事件时间")
 
 if st.sidebar.button("添加倒计时"):
-    if event_name and event_date:
-        SessionState.events[event_name] = event_date
+    if event_name and event_date and event_time:
+        event_datetime = datetime.datetime.combine(event_date, event_time)
+        SessionState.events[event_name] = event_datetime
 
 # 删除事件部分
-st.sidebar.header("删除倒计时")
-event_to_remove = st.sidebar.selectbox("选择要删除的事件", list(SessionState.events.keys()), key="event_to_remove")
+st.header("删除倒计时")
+event_to_remove = st.selectbox("选择要删除的事件", list(SessionState.events.keys()), key="event_to_remove")
 
-if st.sidebar.button("删除倒计时"):
+if st.button("删除倒计时"):
     if event_to_remove:
         del SessionState.events[event_to_remove]
         st.experimental_rerun()
 
 # 显示所有倒计时事件
-st.text("所有倒计时：")
-for event_name, event_date in SessionState.events.items():
-    countdown = calculate_countdown(event_date)
-    st.write(f"距离 **{event_name}** 还有 {countdown} 天")
+st.header("所有倒计时")
+
+for event_name, event_datetime in SessionState.events.items():
+    countdown = calculate_countdown(event_datetime)
+    st.write(f"距离 **{event_name}** 还有 {countdown} 秒")
